abe3c34303e799eb6045038e7f46e66074180009
mirsery <shuang.li@szjlxh.com>
2017-10-31 14:32:08 +0800

    fix the bug

--- src/main/java/cn/szjlxh/push/message/PushMsg.java
+++ src/main/java/cn/szjlxh/push/message/PushMsg.java
@@ -5,17 +5,32 @@ public class PushMsg {
     private String data;
     private int id;
 
-    public PushMsg(String channelId, String data) {
-        this.channelId = channelId;
-        this.data = data;
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
     @Override
     public String toString() {
-        return "{id:\"" + id + "\",channelId:\"" + channelId + "\",data:\"" + data + "\"}";
+        return "{\"id\":" + id + ",\"channelId\":\"" + channelId + "\",\"data\":\"" + data + "\"}";
     }
 }
